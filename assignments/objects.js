e68d5158527f67a7a2fd14166a632bce223db5fd
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-01-15 14:31:09 -0500

    Completed objects challenges + objects stretch challenges

--- assignments/objects.js
+++ assignments/objects.js
@@ -29,45 +29,88 @@ const keven = crtObj('3,kmummery2@wikimedia.org,Keven,M');
 const gannie = crtObj('4,gmartinson3@illinois.edu,Gannie,M');
 const antonietta = crtObj('5,adaine5@samsung.com,Antonietta,F');
 
+
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
 
+console.log(mitzi.name)
+
 // Kennan's ID
+
+console.log(kennan.id)
 
 // Keven's email
 
+console.log(keven.email)
+
 // Gannie's name
+
+console.log(gannie.name)
 
 // Antonietta's Gender
 
+console.log(antonietta.gender)
+
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
-// console.log(kennan.speak());
+
+kennan.speak = () => "Hello, my name is Kennan!";
+
+console.log(kennan.speak());
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
-//console.log(antonietta.multiplyNums(3,4));
+
+antonietta.multiplyNums = (x,y) => 3*4;
+
+console.log(antonietta.multiplyNums(3,4));
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
 // ==== Stretch Challenge: Nested Objects and the this keyword ==== 
 
 // 1. Create a parent object with properties for name and age.  Make the name Susan and the age 70.
+
+const parent = {'name': 'Susan', 'age': 70}
+
 // 2. Nest a child object in the parent object with name and age as well.  The name will be George and the age will be 50.
+
+parent.child = {'name': 'George', 'age': 50}
+
 // 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
+
+parent.child.grandchild = {'name': 'Sam', 'age': 30}
+
 // 4. Give each of the objects the ability to speak their names using the this keyword.
 
-const parent = {}
+parent.speak = () => "Hello, my name is "+parent.name
+
+parent.child.speak = () => "Hello, my name is "+parent.child.name
+
+parent.child.grandchild.speak = () => "Hello, my name is "+parent.child.grandchild.name
 
 // Log the parent object's name
 
+console.log(parent.name)
+
 // Log the child's age
+
+console.log(parent.child.age)
 
 // Log the name and age of the grandchild
 
+console.log(parent.child.grandchild.name+" is "+parent.child.grandchild.age+' years old!')
+
 // Have the parent speak
+
+console.log(parent.speak())
 
 // Have the child speak
 
+console.log(parent.child.speak())
+
 // Have the grandchild speak
+
+console.log(parent.child.grandchild.speak())
+
